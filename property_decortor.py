a3928a2ce1f5cd6a328c2b30d7396b3880389c6b
thanu ganesh <thanuganesh24@gmail.com>
2018-11-11 21:56:47 +0530

    code changes

--- property_decortor.py
+++ property_decortor.py
@@ -1,1 +1,19 @@
 " Thais function helps to learn property decortor"""
+
+
+class Employee:
+    
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+	self.email = first + "." + last + '@emil.com'
+   
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('thanu', 'ganesh')
+
+
+print(emp_1.first)
+print(emp_1.email)
+print(emt_1.fullname())
